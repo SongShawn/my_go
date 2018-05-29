3de8199befdc7f269828a7fcbb252dcbb3da3e04
SongShawn <553815887@qq.com>
2018-05-29 10:13:13 +0800

    vscode summit

--- tflag/main.go
+++ tflag/main.go
@@ -1,1 +1,19 @@
-package tflag
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	xx string
+)
+
+func init() {
+	flag.StringVar(&xx, "xx", "", "xx help")
+	flag.Parse()
+}
+
+func main() {
+	fmt.Println("main")
+}
